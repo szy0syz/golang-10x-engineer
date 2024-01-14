5b31ccb7354d4614d1fe2d9dce6f1c3bea05d90d
Zhenyu Shi <zhenyu.shi@compass.com>
2024-01-14 10:31:39 +0800

    update close channel

--- 010_gift/concurrence/channel_buffer.go
+++ 010_gift/concurrence/channel_buffer.go
@@ -1,4 +1,24 @@
 package main
 
+import "github.com/szy0syz/golang-10x-engineer/gift/database"
+
 func main() {
+	ch := make(chan database.Gift, 100)
+
+	go func() {
+		for i := 0; i < 10000; i++ {
+			ch <- database.Gift{}
+		}
+		close(ch)
+	}()
+
+	for {
+		// 如果读写速率不一致，则会导致这行代码阻塞
+		gift, ok := <-ch
+		if !ok { // ok == false的条件必须满足如下两个条件：1.Channel已空 2.Channel已关闭
+			break // Channel 数据空且已关闭
+		}
+		// 写入redis缓存
+		_ = gift
+	}
 }
